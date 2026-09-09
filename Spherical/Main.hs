7d536d289d1423895f9e94f994a351f1a4954c55
naturefx <alsmrqs@outlook.com>
2026-09-09 08:13:27 -0300

    sintax-adjust

--- Spherical/Main.hs
+++ Spherical/Main.hs
@@ -12,11 +12,11 @@ cross = flip edge
 instance (Show a) => Show (Quartic a) where
     show g = 
         "\n"++
-        show (vert $ cross W up)++ " " ++(show $ vert up)++ " " ++show (vert $ cross E up)
-        ++"\n"++
+        -- show (vert $ cross W up)++ " " ++(show $ vert up)++ " " ++show (vert $ cross E up)
+        -- ++"\n"++
         show (vert $ cross W g) ++ " " ++ (show $ vert g) ++ " " ++ show (vert $ cross E g)
-        ++"\n"++
-        show (vert $ cross W down)++ " " ++(show $ vert down)++ " " ++show (vert $ cross E down)
+        -- ++"\n"++
+        -- show (vert $ cross W down)++ " " ++(show $ vert down)++ " " ++show (vert $ cross E down)
         ++"\n"
         where
         up = cross N g
@@ -27,26 +27,16 @@ data Dir = N | S | W | E
 type Quartic a = Graph a Dir
 
 main :: IO ()
-main = navigate (build [1,2,3] [[4,5,6]]) -- [[4,5,6],[7,8,9]])
+main = navigate (build [1,2,3] [[4,5,6],[7,8,9],[10,11,12]])
     
 navigate :: Quartic Int -> IO ()
-navigate g = do
-    print g
-    input <- getChar
-    Process.system "clear"
-    case input of
+navigate g = (print g >> getChar) >>= \input -> Process.system "clear" 
+    >> case input of
         '8' -> navigate $ cross N g
         '2' -> navigate $ cross S g
         '4' -> navigate $ cross W g
         '6' -> navigate $ cross E g
-        'q' -> return ()
-
-build :: [a] -> [[a]] -> Quartic a
-build (x:xs) ys = node
-    where
-    node          = Vertex x (link up down left right)
-    (left, right) = horizontal xs up down (node, node)
-    (up  , down)  = vertical ys (node, node)
+        _   -> return ()
 
 link :: Quartic a -> Quartic a -> Quartic a -> Quartic a -> (Dir -> Quartic a)
 link up down left right = \dir -> case dir of
@@ -55,20 +45,29 @@ link up down left right = \dir -> case dir of
     W -> left
     E -> right
 
-horizontal :: [a] -> Quartic a -> Quartic a -> (Quartic a, Quartic a) -> (Quartic a, Quartic a)
-horizontal set up down (origin, node)= case set of
-    []     -> (,) node origin
-    (x:xs) -> (,) last next
-        where
-        next          = Vertex x (link (edge up E) (edge down E) node right)
-        (last, right) = horizontal xs (edge up E) (edge down E) (origin, next) 
+build :: [a] -> [[a]] -> Quartic a
+build (x:xs) ys = node
+    where
+    node          = Vertex x (link up down left right)
+    (up  , down)  = vertical   ys (node, node)
+    (left, right) = horizontal xs (node, node)
 
 vertical :: [[a]] -> (Quartic a, Quartic a) -> (Quartic a, Quartic a)
 vertical sets (origin, node) = case sets of
-    []          -> (,) node origin
+    []          -> (,) last origin where last = node
     ((x:xs):ys) -> (,) last next
         where
         next          = Vertex x (link node down left right) 
-        (left, right) = horizontal xs node down (origin, next)
-        (last, down)  = vertical ys (origin, next) 
+        (left, right) = horizontal xs (origin, next)
+        (last, down)  = vertical   ys (origin, next) 
+
+horizontal :: [a] -> (Quartic a, Quartic a) -> (Quartic a, Quartic a)
+horizontal set (origin, node) = case set of
+    []     -> (,) last origin where last = node
+    (x:xs) -> (,) last next
+        where
+        up            = edge node N
+        down          = edge node S
+        next          = Vertex x (link (edge up E) (edge down E) node right)
+        (last, right) = horizontal xs (origin, next) 
 
